d6e3cbe8e7bbd7da5202bbd3fd90dccd7506ef80
HyunjoJeong <twinkleandy@snu.ac.kr>
2024-04-05 20:21:35 +0900

    next.config.js 에서 styledComponents ssr 설정을 true로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
-    styledComponents: {
-      // https://nextjs.org/docs/architecture/nextjs-compiler#styled-components
-      ssr: false, // https://styled-components.com/docs/tooling#server-side-rendering
-    },
+    styledComponents: true, // https://nextjs.org/docs/architecture/nextjs-compiler#styled-components
     removeConsole: process.env.NODE_ENV === "production" && {
       exclude: ["error", "warn"],
     },
