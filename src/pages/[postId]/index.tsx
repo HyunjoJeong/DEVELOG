8603d145cabfc36f5e3ca3edaa5d0a019f112ecd
HyunjoJeong <twinkleandy@snu.ac.kr>
2024-04-05 20:49:29 +0900

    상세 게시글 페이지 생성

---
+++ src/pages/[postId]/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@components/templates/Layout";
+
+type TProps = {};
+
+export default function PostPage({}: TProps) {
+  return (
+    <Layout>
+      <p>이곳은 상세 게시물 페이지 입니다.</p>
+    </Layout>
+  );
+}
