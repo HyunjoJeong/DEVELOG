2458b4b401beb394ced74f323fb811ec1c313a39
HyunjoJeong <twinkleandy@snu.ac.kr>
2024-04-05 20:21:50 +0900

    MainLogo에 priority 설정 추가

--- src/app.components/atoms/common/MainLogo.tsx
+++ src/app.components/atoms/common/MainLogo.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
-export default function MainLogo({ w = 96, h = 40 }) {
-  return <Image src={"/images/mainlogo.png"} alt="로고" width={w} height={h} />;
+export default function MainLogo({ width = 96, height = 40 }) {
+  return (
+    <Image
+      src={"/images/mainlogo.png"}
+      alt="로고"
+      priority
+      width={width}
+      height={height}
+    />
+  );
 }
